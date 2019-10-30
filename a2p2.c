fb73805e7fd7f02db7f9a8539b4472a2690cca85
Zeel Shah <zshah26@uwo.ca>
2019-10-30 11:29:46 -0400

    Done need to clean up and h file

--- a2p2.c
+++ a2p2.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 void quicksort(int sjfq[], char* pq[], int min, int max);
+
 int main()
 {
 
@@ -12,19 +13,23 @@ int *queue = malloc (1000*sizeof(int));
 int *sjfqueue = malloc (1000*sizeof(int));
 char **pqueue = malloc (1000*sizeof(char*));
 char **wqueue = malloc (1000*sizeof(char*));
+int  *turnqueue = malloc (1000*sizeof(int));
+char **turnpqueue = malloc (1000*sizeof(char*));
+int *aqueue = malloc (1000*sizeof(int));
+
+
 int numq=0;
 int nump=0;
 float count=0;
 int tq;
 //int ptime;
-int sum=0;
+int sum;
 float avg;
-//int queuecounter =0;
+
 char* line = NULL;
 size_t lbsize=0;
-ssize_t lsize;
 char *token;
-int filesize;
+
 
 FILE *fp, *fo;
 
@@ -40,14 +45,18 @@ if(fp==NULL)
 // open file to write to 
 fo = fopen( "cpu_scheduling_output_file.txt", "w+" );
 
-
+if(fo==NULL)
+{
+	printf("Error when opening file");
+	return 0;
+}
 
 
 // until the end of the file is reached 
-//while (!feof(fp))
-//{
+while (!feof(fp))
+{
 	// get the line for the file 
-	lsize=getline(&line, &lbsize, fp); 
+	getline(&line, &lbsize, fp); 
 	
 	token = strtok(line," "); // q
 	numq++;// increment the queue number 
@@ -60,25 +69,27 @@ fo = fopen( "cpu_scheduling_output_file.txt", "w+" );
 
 	 
 	 token = strtok(NULL, " "); // read the first process 
+	 
 	while (token!=NULL)
 	{
     
 	pqueue[nump]= token; //  store it in the process queue
+	wqueue[nump]= token;
+	turnpqueue[nump] = token;
+		
 	token = strtok(NULL, " "); // read the first process burst time
 	queue[nump] = atoi(token);//store the burst time 
+	sjfqueue[nump]= atoi(token);
+	turnqueue[nump] = atoi(token);
 	nump++; // increment the number of processes
 	count++; 
 	token = strtok(NULL, " "); // read the next process
 	}
 	
-	
-	   
-		  
+
 		
-		
-		
-		fprintf(fo,"Ready Queue 1 Applying FCFS Scheduling: \n");
-		printf("Ready Queue 1 Applying FCFS Scheduling: \n");
+		fprintf(fo,"Ready Queue %d Applying FCFS Scheduling: \n",numq);
+		printf("Ready Queue %d Applying FCFS Scheduling: \n",numq);
 		fprintf(fo,"Order of selection by CPU: \n");
 		printf("Order of selection by CPU: \n");		
 		
@@ -88,6 +99,7 @@ fo = fopen( "cpu_scheduling_output_file.txt", "w+" );
 		{
 			fprintf(fo,"%s ",pqueue[i]);
 			printf("%s ",pqueue[i]);
+			
 		}
 		
 		
@@ -95,7 +107,7 @@ fo = fopen( "cpu_scheduling_output_file.txt", "w+" );
 		printf("\n\nIndividual waiting time for each process \n");
 		
 		int avgsum=0;
-		
+		sum=0;
 		for(int i=0; i < nump; i++)
 		{
 			fprintf(fo,"%s = %d\n",pqueue[i],sum);
@@ -106,21 +118,20 @@ fo = fopen( "cpu_scheduling_output_file.txt", "w+" );
 			
 		}	
 		
-		avg = avgsum/count;
+		avg = avgsum/((float)nump);
 		fprintf(fo,"\n\nAverage waiting time for each process: %.1f\n",avg);
 		printf("\n\nAverage waiting time for each process: %.1f\n",avg);
 		
 		
 		//SJF Alogrithm
-		sjfqueue=queue;// equate the two queues 
-		wqueue=pqueue;
+		
 		 
 		// do a quick sort and change the values of the processes as well 
 		
 		quicksort (sjfqueue, wqueue, 0,nump-1);
 		
-		fprintf(fo,"\nReady Queue 1 Applying SJF Scheduling: \n");
-		printf("\nReady Queue 1 Applying SJF Scheduling: \n");
+		fprintf(fo,"\nReady Queue %d Applying SJF Scheduling: \n",numq);
+		printf("\nReady Queue %d Applying SJF Scheduling: \n",numq);
 		
 		fprintf(fo,"\nOrder of Selection: \n");
 		printf("\nOrder of Selection: \n");
@@ -145,56 +156,118 @@ fo = fopen( "cpu_scheduling_output_file.txt", "w+" );
 			sum+=sjfqueue[i];
 		}	
 		
-		avg = avgsum/count;
+		avg = avgsum/((float)nump);
 		fprintf(fo,"\n\nAverage waiting time for each process: %.1f\n",avg);
 		printf("\n\nAverage waiting time for each process: %.1f\n",avg);
 		
-	/*	fprintf(fo,"Ready Queue 1 Applying RR Scheduling: \n");
-		printf(fo,"Ready Queue 1 Applying RR Scheduling: \n");
+		fprintf(fo,"\nReady Queue %d Applying RR Scheduling: \n",numq);
+		printf("\nReady Queue %d Applying RR Scheduling: \n",numq);
 		
 		
-		int index, unfinished, tqtotal = 0, bool = 0,wtime = 0, ttime = 0,;
-		int  *tqueue;
-		int *processfinqueue;
-		int *temp; 
+		int unfinished = nump;
+		int more = nump;
+		int tqtotal=0,index=0; 
 		
-		// get the number of processes and set that as the value of unfinished processes
-		unfinished = nump;
-		temp=queue;
+		 
+		int atime=0;
+		
 		
 		for (int i=0; i<nump; i++)
 		{
-			
-			
-		
-		}
-		
-		for(tqtotal=0, index=0; unifinished!=0)
-		{
-			if(*temp<= tq && *temp>0)
+			aqueue[i]= atime;
+			// add the arrival time for each process
+			if(queue[i]<tq)
 			{
-				tqtotal = tqtotal + *temp;
-				*temp = 0; 
+				atime+=queue[i]; //arrival time is less than 4 so add that val
+			}
+			else 
+			{
+				atime += tq;
+			}
+		}	
+			
+		
+		int bool;
+		
+		while (unfinished!=0)
+		{
+			if(queue[index]<= tq && queue[index]>0)
+			{
+				tqtotal = tqtotal + queue[index];// get the total turnaround time
+				queue[index] = 0; 
+				//printf("%s's time is now %d\n",pqueue[index],queue[index]);
 				bool=1;
 			}
-			else if (*temp > 0)
+			else if (queue[index] > 0)
 			{
-				*temp=*temp-tq;
+				queue[index]=queue[index]-tq;
+				pqueue[more] = pqueue[index];
+				more++;
 				tqtotal = tqtotal+tq;
+				}
+			
+			
+			if(queue[index] == 0 && bool ==1)
+			{
+				unfinished --;
+				turnqueue[index] = tqtotal-aqueue[index]; // store the total turnaround tijme for that process
+				turnpqueue [index]= pqueue[index];// store the process turnaround time
+				bool=0;
+			}
+			if(index == nump-1)
+			{
+				index =0;
+			}
+			else if( aqueue[index+1]<=tqtotal)
+			{
+				index++;
+			}
+			else 
+			{
+				index = 0;
 			}
 			
-			if(*temp == 0 && bool ==1)
-			{
-				unifinished --;
-				*tqueue
-		*/
+			//printf("\nthe number of unfinished processed is %d\n",unfinished);
+		}
+		
+		fprintf(fo,"\nOrder of Selection: \n");
+		printf("\nOrder of Selection: \n");
+		
+		for (int i=0; i<more; i++)
+		{
+			fprintf(fo,"%s ",pqueue[i]);
+			printf("%s ",pqueue[i]);
+		}
+		
+		fprintf(fo,"\n\nIndividual turnaround time for each process \n");
+		printf("\n\nIndividual turnaround time for each process \n");
+		
+		sum=0;
+		for(int i=0; i<nump; i++)
+		{
+			
+			fprintf(fo,"%s = %d\n",turnpqueue[i],turnqueue[i]);
+			printf("%s = %d\n",turnpqueue[i], turnqueue[i]);
+			sum+=turnqueue[i];
+		}	
+		
+		nump=0;
+		printf("\n\n");
+		fprintf(fo,"\n\n");
+		
+		
 	
-//}
+}
 free(queue);
+free(pqueue);
+free(sjfqueue);
+free(wqueue);
+free(turnqueue);
+free(turnpqueue);
+free(aqueue);
 fclose(fp);
 fclose(fo);
 			
-		
 	return 0;	
 
 		
